c97fa31ec5f694d574dd9ec13906cba2fe2f9dd6
cyannara <cyannara@naver.com>
2025-05-16 04:55:26 +0900

    vue 배포포

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  console.log("index request")
-  res.send('index')
-})
+app.use(express.static("public"));
 
-app.get('/hello', (req, res) => {
-  console.log(req.url)
-  res.send('Hello World!')
-})
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+
+app.get("/hello", (req, res) => {
+  console.log(req.url);
+  res.send("Hello World!");
+});
+
+app.get("/api/board", (req, res) => {
+  res.send({ title: "Hello World!" });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./public", "index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
