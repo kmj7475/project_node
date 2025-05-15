43203ceebf71a67db80d6efd1a74460923b90460
cyannara <cyannara@naver.com>
2025-05-16 08:19:53 +0900

    deploy

--- frontapp/src/views/HomeView.vue
+++ frontapp/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App 배포 테스트~~" />
+    <HelloWorld msg="Welcome to Your Vue.js App 배포 테스트~~~~" />
   </div>
 </template>
 
