9016de4cd1cdfa77d406da0f629121e107cef9c0
yumi <cyannara@naver.com>
2025-05-16 10:09:19 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 3000;
+
+const path = require('path');
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
 
 app.get("/", function (req, res, next) {
   res.sendFile(path.join(__dirname, "./public", "index.html"));
